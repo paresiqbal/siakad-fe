a8e05ee5ba8c2274385552ac8a40324ddcae9f46
paresiqbal <thecrowdel@gmail.com>
2024-09-07 15:35:06 +0700

    Refactor register page to include registration form and handle form submission

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,11 +1,68 @@
-export default function page() {
+"use client";
+
+import { useState } from "react";
+
+// interface
+interface FormData {
+  username: string;
+  password: string;
+}
+
+interface Errors {
+  username?: string;
+  password?: string;
+}
+
+export default function Register() {
+  const [formData, setFormData] = useState<FormData>({
+    username: "",
+    password: "",
+  });
+
+  const [error, setError] = useState<Errors>({});
+
+  async function handleRegister(e: React.FormEvent) {
+    e.preventDefault();
+
+    const res = await fetch("http://127.0.0.1:8000/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await res.json();
+
+    if (data.errors) {
+      setError(data.errors);
+    } else {
+      console.log(data);
+    }
+  }
+
   return (
     <div>
       <h1>Register Account</h1>
-      <form action="submit">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Confirm Password" />
+      <form onSubmit={handleRegister}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={formData.username}
+          onChange={(e) =>
+            setFormData({ ...formData, username: e.target.value })
+          }
+        />
+        {error.username && <p className="text-white">{error.username}</p>}
+        <input
+          type="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.target.value })
+          }
+        />
         <button type="submit">Register</button>
       </form>
     </div>
