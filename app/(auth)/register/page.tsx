d25323d0a3ffbaf625922d4118f6c8be4047b17f
paresiqbal <thecrowdel@gmail.com>
2024-09-07 14:55:53 +0700

    Refactor register page to include registration form

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,3 +1,13 @@
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div>
+      <h1>Register Account</h1>
+      <form action="submit">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <input type="password" placeholder="Confirm Password" />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 }
