3f62c917b5afb39ac147728e82644e6f5e09951f
paresiqbal <thecrowdel@gmail.com>
2024-09-09 13:56:03 +0700

    Refactor utility function for class merging

---
+++ app/layout/ClientLayout.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import AppProvider from "@/context/AppContext";
+
+export default function ClientLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <AppProvider>{children}</AppProvider>;
+}
