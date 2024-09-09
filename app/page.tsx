fc624288faca2bd788bc73365c79b10e18d7c872
paresiqbal <thecrowdel@gmail.com>
2024-09-09 14:33:14 +0700

    Refactor Home component to include name from AppContext

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import { AppContext } from "@/context/AppContext";
+import { useContext } from "react";
+
 export default function Home() {
+  const context = useContext(AppContext);
+
+  if (!context) {
+    throw new Error("AppContext must be used within an AppProvider");
+  }
+
+  const { name } = context;
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World!</h1>
+      <h1>Hello World! {name}</h1>
     </main>
   );
 }
