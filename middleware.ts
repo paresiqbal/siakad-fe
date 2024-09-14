fa7fc29eee0d6dd1b1d5e18c39935cbf339503e5
paresiqbal <thecrowdel@gmail.com>
2024-09-14 22:48:23 +0700

    asdasda

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,6 @@ export function middleware(req: NextRequest) {
   const token = req.headers.get("authorization")?.split(" ")[1];
 
   if (!token) {
-    // Redirect to login if token is missing
     return NextResponse.redirect(new URL("/login", req.url));
   }
 }
