ff89c45681c0371837c16b7d2f059645fa7a3ae5
paresiqbal <thecrowdel@gmail.com>
2024-09-16 22:34:26 +0700

    Refactor login page to redirect to the dashboard after successful login

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,9 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
   const token = req.headers.get("authorization")?.split(" ")[1];
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+  // if (!token) {
+  //   return NextResponse.redirect(new URL("/login", req.url));
+  // }
 }
 
 export const config = {
