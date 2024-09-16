c257c79350dbebdba7fc0705f9657637d81b18fe
paresiqbal <thecrowdel@gmail.com>
2024-09-16 21:57:56 +0700

    Refactor middleware to use cookies for token retrieval and redirect to login page if token is missing

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const token = req.headers.get("authorization")?.split(" ")[1];
+  const token = req.cookies.get("token");
 
   if (!token) {
     return NextResponse.redirect(new URL("/login", req.url));
