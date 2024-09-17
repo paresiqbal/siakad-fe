6b1fb96d153209a5f99aa9f97df05d5fb177aff5
paresiqbal <thecrowdel@gmail.com>
2024-09-17 20:58:37 +0700

    Refactor authentication middleware and route configuration

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
 import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 const authOptions: NextAuthOptions = {
@@ -52,4 +53,6 @@ const authOptions: NextAuthOptions = {
   },
 };
 
-export default authOptions;
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { signIn } from "next-auth/react";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Landing Page</h1>
+      <button onClick={() => signIn()}>Sign in</button>
     </main>
   );
 }
