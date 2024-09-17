6b87bd85361e773132b5e445f84668a8251e8d46
paresiqbal <thecrowdel@gmail.com>
2024-09-17 22:54:21 +0700

    Refactor layout to include ClientLayout component for session management

---
+++ app/layout/ClientLayout.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+
+export default function ClientLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <SessionProvider>{children}</SessionProvider>
+      </body>
+    </html>
+  );
+}
