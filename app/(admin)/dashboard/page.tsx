1bff0fa866b38434973847cc45f0af274d23c3cd
paresiqbal <thecrowdel@gmail.com>
2024-09-17 19:52:13 +0700

    Refactor package dependencies and remove unused code basic

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,11 +1,6 @@
-import { useSession } from "next-auth/react";
 import React from "react";
 
 export default function AdminDashboard() {
-  // const { data: session, status }: any = useSession();
-  // console.log(session);
-  // console.log(status);
-
   return (
     <div className="p-6">
       <main className="hidden flex-grow overflow-auto p-4 md:block">
